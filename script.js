88eee3e4144b1f2d18d970269f76984e85154f3f
Abhi70815 <abhishekchaurasia45621@gmail.com>
2024-07-08 17:38:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
- const table = document.getElementById('myTable');
-        const promises = [];
-        for (let i = 1; i <= 3; i++) {
-            promises.push(new Promise((resolve) => {
-                const timeout = Math.floor(Math.random() * 3) + 1;
-                setTimeout(() => {
-                    resolve({ promise: `Promise ${i}`, time: timeout });
-                }, timeout * 1000);
-            }));
-        }
+const table = document.getElementById('myTable');
+const promises = [{promise:"Promise 1",time:2},
+				  {promise:"Promise 2", time:1},
+				  {promise:"Promise 3", time:3}];
 
-        Promise.all(promises).then((results) => {
-            const totalTime = results.reduce((acc, { time }) => acc + time, 0);
-            table.innerHTML = '';
+const promiseArray = promises.map((promise)=>{
+    return new Promise((resolve)=>{
+        let time = parseInt(promise.time) * 1000; // convert seconds to milliseconds
+        setTimeout(()=>{
+            resolve({ promise: promise.promise, time: promise.time });
+        }, time);
+    })
+})
 
-            results.forEach(({ promise, time }) => {
-                const row = table.insertRow();
-                row.innerHTML = `
-                    <td>${promise}</td>
-                    <td>${time} seconds</td>
-                `;
-            });
+Promise.all(promiseArray).then((results) => {
+    const totalTime = results.reduce((acc, { time }) => acc + time, 0);
+    table.innerHTML = '';
 
-            const totalRow = table.insertRow();
-            totalRow.innerHTML = `
-                <td>Total</td>
-                <td>${totalTime.toFixed(3)} seconds</td>
-            `;
-        });
+    results.forEach(({ promise, time }) => {
+        const row = table.insertRow();
+        row.innerHTML = `
+            <td>${promise}</td>
+            <td>${time} seconds</td>
+        `;
+    });
+
+    const totalRow = table.insertRow();
+    totalRow.innerHTML = `
+        <td>Total</td>
+        <td>${totalTime.toFixed(3)} seconds</td>
+    `;
+});
